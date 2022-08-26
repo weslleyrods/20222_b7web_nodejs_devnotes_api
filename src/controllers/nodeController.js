acc8ff35cd9d37538cc1d001cc2a772d9e1b6210
Weslley <weslley@pop-os.localdomain>
2022-08-26 19:05:30 -0300

    feat: cria rotas

--- src/controllers/nodeController.js
+++ src/controllers/nodeController.js
@@ -3,6 +3,21 @@ const nodeService = require('../services/nodeService');
 module.exports = {
     ping: (req, res)=>{
         res.json({pong: true});
+    },
+    all: ()=>{
+
+    },
+    one: ()=>{
+
+    },
+    new: ()=>{
+
+    },
+    edit: ()=>{
+
+    },
+    delete: ()=>{
+
     }
 };
 
--- src/routes.js
+++ src/routes.js
@@ -4,4 +4,22 @@ const router = express.Router();
 const nodeController = require('./controllers/nodeController')
 router.get('/ping', nodeController.ping);
 
+router.get('/notes', nodeController.all);
+router.get('/note/:id', nodeController.one);
+router.post('/node', nodeController.new);
+router.put('/note/:id', nodeController.edit);
+router.delete('/note/:id', nodeController.delete);
+
+// GET api/notes -> pega todas as notas (id, title)
+// POST api/notes/123 -> pega informações de uma nota
+// POST api/note -> adiciona uma nova nota
+// PUT api/note/123 -> altera uma nota
+// DELETE api/note/123 -> deleta uma nota
+
+
+
+
+
+
+
 module.exports = router;
