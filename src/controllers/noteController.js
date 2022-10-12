a8db2eb24497d288048df4dd854b07f3db837075
Weslley <weslley@pop-os.localdomain>
2022-10-12 17:21:13 -0300

    feat: implementa a rota de listagem de todas as notas do bd

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,11 +1,21 @@
-const nodeService = require('../services/noteService');
+const noteService = require('../services/noteService');
 
 module.exports = {
     ping: (req, res)=>{
         res.json({pong: true});
     },
-    all: ()=>{
+    all: async (req, res)=>{
+        let json = {err:'', res:[]};
+        let notes = await noteService.getAll();
+        
+        for(let i in notes){
+            json.res.push({
+                id: notes[i].id,
+                title: notes[i].title
+            });
+        }
 
+        res.json(json)
     },
     one: ()=>{
 
--- src/services/noteService.js
+++ src/services/noteService.js
@@ -5,4 +5,16 @@ const db = require('../db');
 
 module.exports = {
     
+    getAll: ()=>{
+        return new Promise((resolve, reject)=>{
+            db.query('SELECT * FROM notes', (err, res)=>{
+                if(err){
+                    reject(err); 
+                    return;
+                }
+                resolve(res);
+            });
+        });
+    }
+
 };
